a6d4c6d54a5ee6d4115a647a542b435f71413c6f
thecuriousones <tamaraleonard46@gmail.com>
2021-04-07 17:39:40 -0400

    update

--- js/index.js
+++ js/index.js
@@ -40,3 +40,62 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav 
+const nav1 = document.createElement('a')
+const nav = document.querySelector('nav')
+nav1.textContent = 'Services'
+nav1.setAttribute('href', '#')
+nav1.classList.add('nav-item-1')
+
+nav.appendChild(nav1)
+
+const nav2 = document.createElement('a')
+nav2.textContent = 'Product'
+nav2.setAttribute('href', '#')
+nav2.classList.add('nav-item-2')
+
+nav.appendChild(nav2)
+
+const nav3 = document.createElement('a')
+nav3.textContent = 'Vision'
+nav3.setAttribute('href', '#')
+nav3.classList.add('nav-item-3')
+
+nav.appendChild(nav3)
+
+const nav4 = document.createElement('a')
+nav4.textContent = 'Features'
+nav4.setAttribute('href', '#')
+nav4.classList.add('nav-item-4')
+
+nav.appendChild(nav4)
+
+const nav5 = document.createElement('a')
+nav5.textContent = 'About'
+nav5.setAttribute('href', '#')
+nav5.classList.add('nav-item-5')
+
+nav.appendChild(nav5)
+
+const nav6 = document.createElement('a')
+nav6.textContent = 'Contact'
+nav6.setAttribute('href', '#')
+nav6.classList.add('nav-item-6')
+
+nav.appendChild(nav6)
+// Nav end
+
+// Header img
+const headerImg = document.getElementById('cta-img');
+headerImg.setAttribute('src', siteContent['cta']['img-src'])
+
+// CTA class
+const ctaContent = document.querySelector('.cta-text')
+ctaContent.getElementsByTagName = ('h1')[0].innerHTML 
+ctaContent.textContent ='Dom Is Awesome'
+document.querySelector('.cta-text').style.fontFamily = 'Bangers'
+document.querySelector('.cta-text').style.letterSpacing = '5px'
+document.querySelector('.cta-text').style.fontSize = '72px'
+document.querySelector('.cta-text').style.MarginBottom = '82px'
+
