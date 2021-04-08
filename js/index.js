63030d8f99f3c27a75d807823436acc86c8b5b39
thecuriousones <tamaraleonard46@gmail.com>
2021-04-07 20:40:44 -0400

    update

--- js/index.js
+++ js/index.js
@@ -41,49 +41,19 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-// Nav 
-const nav1 = document.createElement('a')
-const nav = document.querySelector('nav')
-nav1.textContent = 'Services'
-nav1.setAttribute('href', '#')
-nav1.classList.add('nav-item-1')
+// Nav
+const navAnchors = document.querySelectorAll('nav a')
 
-nav.appendChild(nav1)
+// navAnchors[0].textContent = siteContent['nav']['nav-item-1']
+// navAnchors[1].textContent = siteContent['nav']['nav-item-2']
+// navAnchors[2].textContent = siteContent['nav']['nav-item-3']
+// navAnchors[3].textContent = siteContent['nav']['nav-item-4']
+// navAnchors[4].textContent = siteContent['nav']['nav-item-5']
+// navAnchors[5].textContent = siteContent['nav']['nav-item-6']
 
-const nav2 = document.createElement('a')
-nav2.textContent = 'Product'
-nav2.setAttribute('href', '#')
-nav2.classList.add('nav-item-2')
-
-nav.appendChild(nav2)
-
-const nav3 = document.createElement('a')
-nav3.textContent = 'Vision'
-nav3.setAttribute('href', '#')
-nav3.classList.add('nav-item-3')
-
-nav.appendChild(nav3)
-
-const nav4 = document.createElement('a')
-nav4.textContent = 'Features'
-nav4.setAttribute('href', '#')
-nav4.classList.add('nav-item-4')
-
-nav.appendChild(nav4)
-
-const nav5 = document.createElement('a')
-nav5.textContent = 'About'
-nav5.setAttribute('href', '#')
-nav5.classList.add('nav-item-5')
-
-nav.appendChild(nav5)
-
-const nav6 = document.createElement('a')
-nav6.textContent = 'Contact'
-nav6.setAttribute('href', '#')
-nav6.classList.add('nav-item-6')
-
-nav.appendChild(nav6)
+navAnchors.forEach((anchor, index) => {
+  anchor.textContent = siteContent['nav']['nav-item-'+ (index + 1)]
+})
 // Nav end
 
 // Header img
@@ -92,10 +62,21 @@ headerImg.setAttribute('src', siteContent['cta']['img-src'])
 
 // CTA class
 const ctaContent = document.querySelector('.cta-text')
-ctaContent.getElementsByTagName = ('h1')[0].innerHTML 
-ctaContent.textContent ='Dom Is Awesome'
-document.querySelector('.cta-text').style.fontFamily = 'Bangers'
-document.querySelector('.cta-text').style.letterSpacing = '5px'
-document.querySelector('.cta-text').style.fontSize = '72px'
-document.querySelector('.cta-text').style.MarginBottom = '82px'
+const h1 = ctaContent.querySelector('h1')
+const ctaButton = ctaContent.querySelector('button')
+h1.textContent = siteContent['cta']['h1']
+ctaButton.textContent = siteContent['cta']['button']
+
+// Main Content
+const mainContent = document.querySelector('.top-content')
+const mainTexth4 = mainContent.querySelector('h4')
+const mainTextp = mainContent.querySelector('p')
+// const mainText2h4 = mainContent.querySelector('h4')
+// const mainText2p = mainContent.querySelector('p')
+mainTexth4.textContent = siteContent['main-content']['features-h4']
+mainTextp.textContent = siteContent['main-content']['features-content']
+// mainText2h4.textContent = siteContent['main-content']['about-h4']
+// mainText2p.textContent = siteContent['main-content']['about-content']
+
+
 
